12bc19fb5353fcc001057de21a6ffcbbb919e407
JuliaGinkul <ginkul18@gmail.com>
2022-08-25 13:01:58 +0300

    minor changes

--- DTCD-LiveDashControlPanel/src/components/GraphListSelect.vue
+++ DTCD-LiveDashControlPanel/src/components/GraphListSelect.vue
@@ -80,7 +80,6 @@ export default {
     border: 1px solid var(--border);
     background-color: var(--background_main);
     list-style-type: none;
-    padding-left: 0;
     border-radius: 4.44px;
     margin: 0;
     max-height: 127px;
@@ -94,7 +93,6 @@ export default {
     font-size: 14px;
     color: var(--text_secondary);
     padding: 7px 10px;
-    // border-bottom: 1px solid var(--border);
     cursor: pointer;
     display: flex;
     align-items: center;
